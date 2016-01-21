bdca1ea0666fe1ce4ac583beb95bb45def44ad97
Stephan Leroux <sleroux@mozilla.com>
2016-01-21 10:49:54 -0500

    No Bug - Fixed exported Git l10n repo path to work with scripts

--- scripts/import-locales.sh
+++ scripts/import-locales.sh
@@ -12,7 +12,7 @@ if [ -d firefox-ios-l10n ]; then
   fi
 fi
 echo "Creating firefox-ios-l10n Git repo"
-svn export --force https://github.com/mozilla-l10n/firefoxios-l10n firefox-ios-l10n || exit 1
+svn export --force https://github.com/mozilla-l10n/firefoxios-l10n/trunk firefox-ios-l10n || exit 1
 
 #
 # TODO Add incomplete locales here that are NOT to be included.
@@ -42,7 +42,7 @@ fi
 scripts/update-xliff.py firefox-ios-l10n || exit 1
 
 # Remove unwanted sections like Info.plist files and $(VARIABLES)
-scripts/xliff-cleanup.py firefox-ios-l10n/*/*/*.xliff || exit 1
+scripts/xliff-cleanup.py firefox-ios-l10n/*/*.xliff || exit 1
 
 # Export xliff files to individual .strings files
 rm -rf localized-strings || exit 1
