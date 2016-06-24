8773a8e3515d41f23d9e2a6a7c4dd687a452df7c
Stefan Arentz <stefan@arentz.ca>
2016-06-24 10:34:06 -0400

    No Bug - Improved virtualenv creation in import-locales.sh

--- scripts/import-locales.sh
+++ scripts/import-locales.sh
@@ -5,6 +5,22 @@ if [ ! -d Client.xcodeproj ]; then
     exit 1
 fi
 
+SDK_PATH=`xcrun --show-sdk-path`
+
+# If the virtualenv with the Python modules that we need doesn't exist,
+# or a clean run was requested, create the virtualenv.
+if [ ! -d import-locales-env ] || [ "${clean_run}" = true ]
+then
+    rm -rf import-locales-env || exit 1
+    echo "Setting up new virtualenv..."
+    virtualenv import-locales-env --python=python2.7 || exit 1
+    source import-locales-env/bin/activate || exit 1
+    # install libxml2
+    CFLAGS=-I"$SDK_PATH/usr/include/libxml2" LIBXML2_VERSION=2.9.2 pip install lxml || exit 1
+else
+    echo "Reusing existing virtualenv found in import-locales-env"
+fi
+
 # Using svn export to get a version of the Git repo so we can use --force
 # to make rerunning easy and it also allows us to ensure that the repo
 # doesn't get treated as a submodule by Git
