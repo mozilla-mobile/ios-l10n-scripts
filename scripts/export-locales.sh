a3a8b9a2bb9369148a08ed5eb8534d90084dec2a
Stefan Arentz <stefan@arentz.ca>
2016-06-24 11:41:27 -0400

    No Bug - Consistent naming of export-locales-env

--- scripts/export-locales.sh
+++ scripts/export-locales.sh
@@ -46,8 +46,8 @@ if [ ! -d export-locales-env ] || [ "${clean_run}" = true ]
 then
     rm -rf export-locales-env || exit 1
     echo "Setting up new virtualenv..."
-    virtualenv python-env --python=python2.7 || exit 1
-    source python-env/bin/activate || exit 1
+    virtualenv export-locales-env --python=python2.7 || exit 1
+    source export-locales-env/bin/activate || exit 1
     # install libxml2
     CFLAGS=-I"$SDK_PATH/usr/include/libxml2" LIBXML2_VERSION=2.9.2 pip install lxml || exit 1
 else
