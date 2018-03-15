32a4906fe30b32d43cbffd54e6dc6babfe87abc1
Stefan Arentz <stefan@arentz.ca>
2018-03-15 15:19:18 -0400

    Pin lxml to 4.1.1

--- import-locales-firefox.sh
+++ import-locales-firefox.sh
@@ -16,7 +16,7 @@ then
     virtualenv import-locales-env --python=python2.7 || exit 1
     source import-locales-env/bin/activate || exit 1
     # install libxml2
-    CFLAGS=-I"$SDK_PATH/usr/include/libxml2" LIBXML2_VERSION=2.9.2 pip install lxml || exit 1
+    CFLAGS=-I"$SDK_PATH/usr/include/libxml2" LIBXML2_VERSION=2.9.2 pip install lxml==4.1.1 || exit 1
 else
     echo "Reusing existing virtualenv found in import-locales-env"
     source import-locales-env/bin/activate || exit 1
